8fa6f3b0331241ffb8c71f657a1234c140f366af
Alejandro Rivera <juan.alejorivera25@gmail.com>
2025-09-30 21:28:21 -0500

    cors implementation

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import  express  from 'express';
 import dontenv from "dotenv";
+import cors from 'cors';
+import { corsConfig } from "./config/cors";
 import { conenctDB } from './config/db';
 import cousesRoutes from './routes/coursesRoutes';
 
@@ -9,6 +11,7 @@ dontenv.config();
 conenctDB();
 
 const app = express();
+app.use(cors(corsConfig));
 
 app.use(express.json());
 
