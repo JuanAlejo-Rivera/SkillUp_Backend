e521eedf59149746f2a68d0088d8211bd9dd4e75
Alejandro Rivera <juan.alejorivera25@gmail.com>
2025-12-03 18:04:34 -0500

    fixed error cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,28 +1,19 @@
-import { CorsOptions } from "cors";
+import {CorsOptions} from 'cors'
 
-export const corsConfig: CorsOptions = {
-    origin: function (origin, callback) {
-        const whitelist = [
-            process.env.FRONTEND_URL, 
-            process.env.FRONTEND_PROD_URL
-        ];
+export const corsConfig: CorsOptions ={
+    origin: function (origin, callback){
+        //Perimitidos
+        const whitelist = [process.env.FRONTEND_URL, process.env.FRONTEND_PROD_URL];// el undefined es para permitir las peticiones desde postman 
 
-        const isDevAPI = process.argv.includes("--api");
+        if(process.argv[2] === '--api'){
+            whitelist.push(undefined)
+        }
 
-        if (isDevAPI) {
-            // DESARROLLO → permitir Postman (origin === undefined)
-            if (!origin || whitelist.includes(origin)) {
-                return callback(null, true);
-            } else {
-                return callback(new Error("No permitido por CORS"));
-            }
-        } else {
-            // PRODUCCIÓN → bloquear Postman
-            if (origin && whitelist.includes(origin)) {
-                return callback(null, true);
-            } else {
-                return callback(new Error("No permitido por CORS"));
-            }
+        // Permitir si está en la whitelist o si origin es undefined
+        if(!origin || whitelist.includes(origin)){
+                callback(null, true)
+        }else{
+            callback(new Error('No permitido por CORS'))
         }
     }
-};
+}
